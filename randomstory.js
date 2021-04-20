e769e29a8f7e32f8aed93fd81072b5df97a22e94
jace-robin <jace.robin.cov@gmail.com>
2021-04-20 21:54:17 +0000

    update names

--- randomstory.js
+++
@@ -1,8 +0,0 @@
-var name = ["Garry", "Jerry", "Terry", "Matthew"]
-var location = ["park", "school", "IHOP", "gym"]
-var activity = ["chill", "eat", "fight", "jog"]
-function getRandom(input) {
-    return input[(Math.ceil(Math.random() * 4) - 1)];
-};
-
-console.log (getRandom(name) + " went to the " + getRandom(location) + " to " + getRandom(activity) + ", but " + getRandom(name) + " stopped him, and they started to " + getRandom(activity));
