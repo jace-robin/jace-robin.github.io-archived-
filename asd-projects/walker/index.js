869c08356e64b5244db856acff9128b319cecaf4
jace.robin.cov <jace.robin.cov@outlook.com>
2021-06-09 22:03:40 -0500

    IM AMAZING, LETS GOOOOOOOOOOO

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,7 +10,7 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  var rateOfDecrease = 2;
+  var rateOfDecrease = .5;
 var keyarray =
 [
 "~", 
@@ -236,6 +236,7 @@ var keyarray =
 "close bracket", 
 "single qoute", 
 ];
+var keysHeld = [];
   // Game Item Objects
   var player1 = {
     speed : {
@@ -244,13 +245,22 @@ var keyarray =
     },
     pos : {
       x: 0,
-      y: 0
+      y: 0,
     },
     permissions : {
-      allowMove: true
-    },
+      allowMove: true,
+    }
     };
-
+    var coords = {
+      delta : {
+      x: 0,
+      y: 0,
+      },
+      current : {
+      x: 0,
+      y: 0,
+      }
+    };
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
@@ -270,89 +280,154 @@ var keyarray =
    //reduceSpeed(rateOfDecrease);
    //collision()
    updateData();
+  }
   /* 
   Called in response to events.
   */
-  }
-  function updateData() {
-    
-  }
   function handleKeyDown(event) {
     //if (keyarray[event.which - 1].fired === false) {
     var key = keyarray[event.which - 1];
-    console.log (key + " key pressed");
-    keymovement(key);
+    //console.log (key + " key pressed");
+    if (keysHeld.includes (key) === false) {
+      keysHeld.push(key);
+      console.log(keysHeld);
+    }
+    else {
+
+    }
+    //keymovement(key);
       //keyarray[event.which - 1].fired = true;
     //}
 
   };
   function handleKeyUp(event) {
-    console.log (keyarray[event.which - 1] + " key released");
+  //console.log (keyarray[event.which - 1] + " key released");
+    var key = keyarray[event.which - 1];
+    if (keysHeld.includes (key) === true) {
+      keysHeld.splice(keysHeld.indexOf(key), 1);
+      } 
+      console.log(keysHeld);
   };
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
   function consoleLogPosition () {
-    console.log("x: " + player1.pos.x + "y: " + player1.pos.y);
+    coords.delta.x = player1.pos.x;
+    coords.delta.y = player1.pos.y;
+//
+    coords.current.x = player1.pos.x;
+    coords.current.y = player1.pos.y;
+//
+    requireLog = function () {
+      if (coords.delta.x !== coords.current.x || coords.delta.y !== coords.current.y) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    if (requireLog === true) {
+      console.log("x: " + player1.pos.x + ", y: " + player1.pos.y);
+    }
+    else {
+      "debug failed"
+    }
   };
-  function keymovement (key) {
-    if (key === "left" || key === "right" || key === "up" || key === "down") {
-      console.log("function movement called");
-      //if (Math.abs(player1.speed.x) < 10) {
+  function keymovement () {
+    if (keysHeld.includes("left") === true || keysHeld.includes("right") === true || keysHeld.includes("up") === true || keysHeld.includes("down") === true) {
+/*      console.log("function movement called(keymovement)");
+      if (Math.abs(player1.speed.x) < 10) {
           if (key === "left") {
-            player1.speed.x = 2;
+            player1.speed.x = -2;
         }
           else if (key === "right") {
             player1.speed.x = 2;
         }
-      //}
-      //  if (Math.abs(player1.speed.y) < 10) {
-          else if (key === "up") {
+        else {
+          player1.speed.x = 0;
+        }
+      }
+      if (Math.abs(player1.speed.y) < 10) {
+          if (key === "up") {
             player1.speed.y = 2;
         }
           else if (key === "down") {
-            player1.speed.y = 2;
+            player1.speed.y = -2;
         }
-    //}
+      }
+        else {
+          player1.speed.y = 0;
+        }
   }
     else {
-      console.log("nah lmfao");
+      console.log("nah lmao");
+  };*/
+  //new input system for multiple input registration
+  if (Math.abs(player1.speed.x) < 10) {
+    if (keysHeld.includes("left") === true) {
+      player1.speed.x -= 2;
+        }
+    else if (keysHeld.includes("right") === true) {
+      player1.speed.x += 2;
+        }
+    else {
+      //player1.speed.x = 0;
+        }
+      }
+  if (Math.abs(player1.speed.y) < 10) {
+    if (keysHeld.includes("up") === true) {
+      player1.speed.y += 2;
+      }
+    else if (keysHeld.includes("down") === true) {
+      player1.speed.y -= 2;
+      }
     }
-    updatePosition();
-    consoleLogPosition();
-    console.clear;
-};
-  /*function reduceSpeed (ROD) {
+    else {
+        //player1.speed.y = 0;
+      }
+}
+//  else {
+//    console.log("nah lmao");
+//}
+}
+  function reduceSpeed (ROD) {
         if (player1.speed.x < 0) {
           player1.speed.x += ROD;
         }
-        else if (player1.speed.x > 0) {
+        if (player1.speed.x > 0) {
           player1.speed.x -= ROD;
-        };
+        }
 
         if (player1.speed.y < 0) {
           player1.speed.y += ROD;
         }
-        else if (player1.speed.y > 0) {
+        if (player1.speed.y > 0) {
           player1.speed.y -= ROD;
-        };
-  };*/
-  function collision (x, y) {
+        }
+  };
+  /*function collision (x, y) {
         if (player1.positionX <= window.width) {
           player1.xPos -= speedX
         }
-  }
-  function redrawPosition () {
-    $('gameItem').css('left', player1.pos.x);
-    $('gameItem').css('top', player1.pos.y);
+  }*/
+  function updateData() {
+    updatePosition();
   }
   //update the players position in memory,not on screen just yet
   function updatePosition() {
+    keymovement();
     player1.pos.x += player1.speed.x;
-    player1.pos.x += player1.speed.y;
-    collision();
+    player1.pos.y += player1.speed.y;
+    reduceSpeed(rateOfDecrease);
+    //collision();
     redrawPosition();
     consoleLogPosition();
+  }
+  function redrawPosition () {
+    var x = player1.pos.x;
+    var y = player1.pos.y;
+    $(gameItem).css('left', x);
+    $(gameItem).css('top', -y);
   }
   function endGame() {
     // stop the interval timer
