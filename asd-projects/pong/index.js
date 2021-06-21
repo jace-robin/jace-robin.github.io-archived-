a17e0cd342519973959e68d4748a19c899768a0b
jace.robin.cov <jace.robin.cov@outlook.com>
2021-06-20 21:20:38 -0500

    just another checkpoint i guess not close to done, have to finish for the night

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,31 +10,39 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  var boardWidth = $("board").css("width");
-  var boardHeight = $("board").css("height");
+  var boardWidth = $("#board").css("width");
+  var boardHeight = $("#board").css("height");
   var gameInfo = {
     paddleSpeed: 10,
     ballSpeed: 15,
-    spawns: {
-      ball: boardWidth / 2,
-      player: boardWidth - 25,
-      enemy: boardWidth + 25,
+    screen: {
   }
 }
   // Game Item Objects
   var keysHeld = [];
   var player = {
-    x: undefined,
-    y: undefined,
+    id: "#player",
+    x: 10,
+    y: 220,
+    speedX: 0,
+    speedY: 0,
   }
-
+  var enemy = {
+    id: "#enemy",
+    x: 430,
+    y: 220,
+    speedX: 0,
+    speedY: 0,
+  }
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle 
-  $(document).on('keyup', handleKeyUp);  
-  spawnObject (player);
-  spawnObject (enemy);
-  spawnObject(ball);
+  $(document).on('keyup', handleKeyUp); 
+  //spawn positioning
+  player.x = 10; 
+  player.y = 220;
+  enemy.x = 380;
+  enemy.y = 220;
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -44,51 +52,55 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    //update positions in memory
-    //handle any extra collisions or
-    //update positions on screen
+    handlePlayer();
+    handleEnemy();
+    handleBall();
+    updateScreen();
   }
 
   /* 
   Called in response to events.
   */
   function handleKeyDown(event) {
-    var key = keyarray[event.which - 1];
+    var key = keycodes[event.which - 1];
     if (!keysHeld.includes(key)) {
       keysHeld.push(key);
       console.log(keysHeld);
     }
-    else {
-
-    }
   };
   function handleKeyUp(event) {
-      var key = keyarray[event.which - 1];
-      if (keysHeld.includes(key)) {
-        keysHeld.splice(keysHeld.indexOf(key), 1);
-        console.log(keysHeld);
+    var key = keycodes[event.which - 1];
+    if (keysHeld.includes(key)) {
+      keysHeld.splice(keysHeld.indexOf(key), 1);
+      console.log(keysHeld);
         }
     };
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  function teleport ($id, x, y) {
-    var id = $id;
-    $(id).css("left") = gameInfo.spawns.object[id];
-    $(id).css("top") = boardHeight / 2;
+  //FINISH THIS TMMRW MORNING, JUST ADD THE 
+  function handlePlayer() {
+    if (keysHeld.includes("up")) {
+      player.speedX = -5
+    }
+  };
+
+  function updateScreen() {
+    moveObject(player.id, player.x, player.y);
+    moveObject(enemy.id, enemy.x, enemy.y);
   }
-  function findSides (object) {
+  function moveObject (object, x, y) {
+    $(object).css("left", x);
+    $(object).css("top", y);
+  };
+  /*function findSides (object) {
     object.boundary = {
       right,
       left,
       top,
       bottom,
     }
-  }
-  function movePlayer () {
-
-  }
-  
+  };*/
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
--- movementandkeydetection.js
+++ movementandkeydetection.js
@@ -1,5 +1,19 @@
+///////////////////////
+//Entity Declaration//
+//////////////////////
+var object = {
+  speedX: undefined,
+  speedY: undefined,
+  x: undefined,
+  y: undefined,
+  controller: undefined,
+  type: undefined,
+}
+//////////////////
+//Key Detection//
+/////////////////
 function handleKeyDown(event) {
-    var key = keyarray[event.which - 1];
+  var key = keyarray[event.which - 1];
     if (!keysHeld.includes(key)) {
       keysHeld.push(key);
       console.log(keysHeld);
@@ -8,10 +22,17 @@ function handleKeyDown(event) {
 
     }
   };
-  function handleKeyUp(event) {
+function handleKeyUp(event) {
       var key = keyarray[event.which - 1];
       if (keysHeld.includes(key)) {
         keysHeld.splice(keysHeld.indexOf(key), 1);
         console.log(keysHeld);
         }
     };
+/////////////////////
+//Movement And Drag//
+////////////////////
+addVelocity ($id, x, y) {
+  var object = $id;
+
+}
