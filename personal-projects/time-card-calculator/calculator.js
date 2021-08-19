417a12bc1698688c036379d6055f218add54ec33
jace-robin <jace.robin.cov@gmail.com>
2021-08-19 13:17:43 +0000

    calculator ui change

--- personal-projects/time-card-calculator/calculator.js
+++ personal-projects/time-card-calculator/calculator.js
@@ -25,13 +25,17 @@ function calculatePay(hours) {
     return (Math.round((hours * payRate) + ((totalOvertime * payRate) * (payRate * overtimeRate))));
 }
 function result (hours, pay) {
-    $("#totalHours").text = ("$" + hours + " total hours worked");
-    $("#totalPay").text = ("$" + pay + " gross pay earned");
-    alert(hours + " total hours worked.");
-    alert(pay + " total pay.");
+    $("#totalHours").text(hours + " total hours worked");
+    $("#totalPay").text("$" + pay + " gross pay earned");
 }
+//I need to get my order of operations working
+//Not sure if its eaier to convert it to rounded before subtracting or if i should even round at all, and let the user do it instead.
+//If i do then i would have to find out how many hours seperate from minutes and divide by 60
+//might have to do that since its subtracting 60's not 100's, might be messing up the time increments
+//new system might be needed, arrays to hold individual digits before moving on.
 function calculateHours() {
-    return (((out1 - in1) + (out2 - in2)) / 100);
+    var time = ((out1 - in1) + (out2 - in2)) / 100
+    return ((Math.round(time * 4) / 4).toFixed(2));
 }
 function idek() {
     var hours = calculateHours(in1, out1, in2, out2);
