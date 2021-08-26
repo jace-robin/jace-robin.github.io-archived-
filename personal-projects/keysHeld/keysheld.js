e82dfbcbddf11f85e1ab9633b93be289ef635198
jace-robin <jace.robin.cov@gmail.com>
2021-08-26 16:22:34 +0000

    second update keyheld

--- personal-projects/keysHeld/keysheld.js
+++ personal-projects/keysHeld/keysheld.js
@@ -1,78 +1,35 @@
 $(document).ready(runProgram);
-
+var held = [];
 function runProgram() {
-    c.text('keys', "hi");
-    alert(h.quote("hi2"));
 $(document).on('keydown', handleKeyDown);
 $(document).on('keyup', handleKeyUp);
-///////////////////
-//Object Methods//
-//////////////////    
-    var c = {
-        //change methods
-        text: function (id, desired) {
-            //change the text of id
-            $(h.id(id)).text(h.quote(desired));
-        },
-        color: function (id, color) {
-            //change color of id to color input
-            $(h.id(id)).css("color", color);
-        },
-        x: function (id, x) {
-            $(h.id(id)).css("left", h.quote(x));
-        }
-    };
-    var h = {
-        //helper methods
-        quote: function (text) {
-            //add quotations to input
-            return ('"' + text + '"');
-        },
-        id: function (text) {
-            //add quotations and '#' to input
-            return ('"#' + text + '"');
-        },
-    };
-    var u = {
-        //utility methods
-        h: function () {
-            //held keys
-        },
-    };
-////////////////////
-//Var Declarations//
-////////////////////
-var held = [];
-///////////////
-//Update Text//
-///////////////
-    function getKeys() {
-        for (var i = 0; i >= Object.keys(keyStatus).length; i++) {
-            var ii
-            keysHeld[i]=keyStatus[keyObject[i]];
-            if (keyStatus[keyObject[i]]) {
-
-            }
-        };
-    };
-
-    function handleKeyDown(event) {
-        var key = (keyObject[event.which]);
-        keyStatus[key] = true;
-        if (!held.contains(key)) {
-            held.push(key);
-        };
-        alert(key);
-      };
-    function handleKeyUp(event) {
-        var key = (keyObject[event.which]);
-        keyStatus[key] = false;
-        if (held.contains(key)) {
-            held.splice(held.indexOf(key), 1);
-        };
-        alert("unpress");
-      };
 ////////////////
 //Program Loop//
 ////////////////
 };
+
+///////////////////////////////////////////////////////
+
+
+////////////////
+//Input Events//
+////////////////
+function handleKeyDown(event) {
+    var key = (keyObject[event.which]);
+    keyStatus[key] = true;
+    if (!held.includes(key)) {
+        held.push(key);
+    };
+    $("#keys").text(held);
+  };
+function handleKeyUp(event) {
+    var key = (keyObject[event.which]);
+    keyStatus[key] = false;
+    if (held.includes(key)) {
+        held.splice(held.indexOf(key), 1);
+    };
+    $("#keys").text(held);
+  };
+///////////////////
+//Object Methods//
+//////////////////
