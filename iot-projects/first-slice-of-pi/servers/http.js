1e77030a46036dcae64e6bdad5310dac3b21cbd9
covingtonhighunblocked <covingtonhighunblocked@gmail.com>
2021-11-18 17:41:09 -0600

    http push

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,12 @@
 const express = require('express'),
 	cors = require('cors');
-	
-
+var app = express();
+app.use(cors());
 module.exports = app;
+app.get('/', function (req, res) {
+	res.send('Some response for accessing the root');
+});
+app.get('/pi', function(req, res){
+    res.send('Some response for accessing the root');
+});
+// I have looked through all files ip: 192.168.68.249
