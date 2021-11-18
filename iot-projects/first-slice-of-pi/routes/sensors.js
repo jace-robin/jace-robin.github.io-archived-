2bb14c4ebfa0c9b0651dd7e0c5178a98de976324
covingtonhighunblocked <covingtonhighunblocked@gmail.com>
2021-11-18 17:51:28 -0600

    next

--- iot-projects/first-slice-of-pi/routes/sensors.js
+++ iot-projects/first-slice-of-pi/routes/sensors.js
@@ -1,6 +1,22 @@
 const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
+router.route('/').get(function (req, res, next) {
+	res.send(resources.pi.sensors);
+});
+router.route('/dht').get(function (req, res, next) {
+	res.send(resources.pi.sensors.dht);
+});
+router.route('/dht/temperature').get(function (req, res, next) {
+	res.send(resources.pi.sensors.dht.temperature);
+});
+router.route('/dht/humidity').get(function (req, res, next) {
+	res.send(resources.pi.sensors.dht.humidity);
+});
+
+router.route('/pir').get(function (req, res, next) {
+	res.send(resources.pi.sensors.pir);
+});
 
 
 
