2cb069ab87d44af39e42859f2187b1f2280f4ef9
covingtonhighunblocked <covingtonhighunblocked@gmail.com>
2021-11-18 18:16:24 -0600

    next

--- iot-projects/first-slice-of-pi/plugins/internal/pirPlugin.js
+++ iot-projects/first-slice-of-pi/plugins/internal/pirPlugin.js
@@ -4,3 +4,17 @@ const Gpio = require('onoff').Gpio;
 let sensor;
 const device = resources.pi.sensors.pir;
 
+function connectHardware() {
+    sensor = new Gpio(device.gpio, 'in', 'both')
+    sensor.watch(function (err, value) {
+        if (!err) {
+            device.value = !!value;
+        }
+    });
+}
+exports.start = function (params) {
+    connectHardware()
+}
+exports.stop = function () {
+    sensor.unexport()
+}
--- iot-projects/first-slice-of-pi/wot-server.js
+++ iot-projects/first-slice-of-pi/wot-server.js
@@ -5,6 +5,9 @@ const server = httpServer.listen(resources.pi.port, function () {
 	console.log("Running the Pi on port " + resources.pi.port);
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
+	pirPlugin.stop();
 	process.exit();
 });
+var pirPlugin = require('./plugins/internal/pirPlugin');
+pirPlugin.start({});
