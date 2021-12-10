af17586caa20ac249aa8c71c641dccf94eb80d20
covingtonhighunblocked <covingtonhighunblocked@gmail.com>
2021-12-09 18:10:10 -0600

    new

--- iot-projects/second-slice-of-pi/routes/actuators.js
+++ iot-projects/second-slice-of-pi/routes/actuators.js
@@ -2,13 +2,16 @@ const express = require('express'),
 	router = express.Router(),
 	resources = require('./../resources/model');
 router.route('/').get(function (req, res, next) {
-	res.send(resources.pi.actuators);
+	res.result = (resources.pi.actuators);
+	next()
 });
 router.route('/leds').get(function (req, res, next) {
-	res.send(resources.pi.actuators.leds);
+	res.result = (resources.pi.actuators.leds);
+	next()
 });
 router.route('/leds/:id').get(function (req, res, next) {
-	res.send(resources.pi.actuators.leds[req.params.id]);
+	res.result = (resources.pi.actuators.leds[req.params.id]);
+	next()
 });
 
 
