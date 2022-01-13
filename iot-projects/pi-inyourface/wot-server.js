de4fa1b6ebda2ccc721ea02b97f67c4250fccf0d
jace-robin <jace.robin.cov@gmail.com>
2022-01-13 23:19:43 +0000

    new

--- iot-projects/pi-inyourface/wot-server.js
+++ iot-projects/pi-inyourface/wot-server.js
@@ -1,18 +1,22 @@
+const ledsPlugin = require('./plugins/internal/ledsPlugin');
+var pirPlugin = require('./plugins/internal/pirPlugin');
+var dhtPlugin = require('./plugins/internal/dhtPlugin');
 const httpServer = require('./servers/http'),
 	resources = require('./resources/model');
-
 const server = httpServer.listen(resources.pi.port, function () {
 	console.log("Running the Pi on port " + resources.pi.port);
 });
 
+
 process.on('SIGINT', function () {
 	pirPlugin.stop();
 	dhtPlugin.stop();
+	ledsPlugin.stop()
 	process.exit();
 });
-var pirPlugin = require('./plugins/internal/pirPlugin');
-var dhtPlugin = require('./plugins/internal/dhtPlugin');
+
 pirPlugin.start({});
 dhtPlugin.start({
 	'frequency': 2000
 });
+ledsPlugin.start()
