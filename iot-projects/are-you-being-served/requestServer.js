4fc3af36624e29a52e6a2465c3ee378e6c3ec048
jace-robin <jace.robin.cov@gmail.com>
2022-01-13 22:41:39 +0000

    new

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -4,7 +4,7 @@ const request = require('request');
 var port = 8686;
 var args = process.argv.slice(2);
 http.createServer(function(req,res) {
-    var url = args[0] ? args[0] : "https://jace-robin.github.io/portfolio.html";
+    var url = args[0] ? args[0] : "https://github.com/UBG100/ubg100.github.io";
     request(url, function(error, response, body) {
         if (!body || !response || (error === null && response.statusCode !== 200)) {
             res.end("bad URL\n");
